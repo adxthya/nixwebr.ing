4c01e831c967805260f0c774bdabcffcd4cd7602
Adithya K B <adithyakb93@gmail.com>
2024-11-01 00:23:39 +0530

    add adxthya

--- webring.nix
+++ webring.nix
@@ -16,4 +16,5 @@
     { name = "cutedog5695"; site = "https://cutedog5695.me"; config = "https://github.com/CuteDog5695/nixos-config"; }
     { name = "bwkam"; site = "https://bwkam.github.io"; config = "https://github.com/bwkam/dots"; }
     { name = "MarkAssPandi"; site = "https://markasspandi.pl"; config = "https://codeberg.org/MicroPanda123/nixosos"; }
+    { name = "adxthya"; site = "https://adxthya.github.io/"; config = ""; }
 ]
